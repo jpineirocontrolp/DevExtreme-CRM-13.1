9454dce4f0acc16246b1eb7eda73e3b9cce4fad9
Aleksey Martynov <bclexa@gmail.com>
2013-10-22 16:42:07 +0400

    CRM.DataService project

---
+++ CRM.DataService/DataModelExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.DataService {
+    //// Uncomment following class, rename it and add addributes to the corresponding contextual model
+    ////<EntityType Name="Product" m:HasStream="true" xmlns:m="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata">
+    //public partial class Product: IImageSource {
+    //	public byte[] Image {
+    //		get { return ImageSource.Image; }
+    //	}
+    //}
+}
---
+++ CRM.DataService/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace CRM.DataService {
+    public class Global : System.Web.HttpApplication {
+
+        protected void Application_BeginRequest(object sender, EventArgs e) {
+            CRM.DataService.CorsSupport.HandlePreflightRequest(HttpContext.Current);
+        }
+
+    }
+}
